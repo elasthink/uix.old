eab3e476aa567ccfff58ce1fe96b2524ee52085d
angel.teran <angel.teran@elasthink.com>
2018-10-17 17:55:02 +0200

    New reload function in Viewport object, check connection in uix.load() and improvements to the Toast widget.

--- web/views/widgets/toast/handler.js
+++ web/views/widgets/toast/handler.js
@@ -13,6 +13,36 @@ View.define('widgets/toast', {
         root.querySelector('.toast1').addEventListener('tap', function() {
             uix.showToast('No por mucho madrugar amanece más temprano');
         });
+        root.querySelector('.toast2').addEventListener('tap', function() {
+            // Usando un fragmento y añadiendo los nodos directamente:
+            /*
+            var frag = document.createDocumentFragment(), a;
+            frag.appendChild(document.createTextNode('We\'re sorry, an error occurred. '));
+            frag.appendChild(a = document.createElement('a'));
+            a.appendChild(document.createTextNode('TRY AGAIN'));
+            a.addEventListener('tap', function() {
+                alert('Trying again...');
+            });
+            frag.appendChild(document.createTextNode('.'));
+            uix.showToast(frag);
+            */
+
+            // Usando nuestra función uix.create():
+            uix.showToast([
+                'Something went wrong. ',
+                uix.create('a', {}, 'Try Again', {
+                    'tap': function() {
+                        alert('Trying again...');
+                    }
+                }), '.'
+            ]);
+        });
+        root.querySelector('.toast3').addEventListener('tap', function() {
+            uix.showToast('<b>No internet connection</b><br/>Please check your connection and try again.');
+        });
+        root.querySelector('.toast4').addEventListener('tap', function() {
+            uix.showToast('<b>Something went wrong</b><br/>Please try again later.');
+        });
     }
 
 });
